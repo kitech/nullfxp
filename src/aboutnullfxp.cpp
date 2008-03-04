7a36b796547fbcbdf129eb1fc8b33f3b26c58900
liuguangzhao <liuguangzhao@localhost>
2008-03-04 05:47:06 +0000

    add qt version in about dialog

--- src/aboutnullfxp.cpp
+++ src/aboutnullfxp.cpp
@@ -26,7 +26,8 @@
 AboutNullFXP::AboutNullFXP(QWidget* parent, Qt::WindowFlags f): QDialog(parent, f)
 {
     ui_about_nullfxp.setupUi(this);
-    this->ui_about_nullfxp.label_2->setText( NULLFXP_RELEASE );
+    QString NULLFXP_RELEASE_AND_QT_VERION = QString("%1 (Using Qt %2)").arg(NULLFXP_RELEASE).arg(QT_VERSION_STR);
+    this->ui_about_nullfxp.label_2->setText( NULLFXP_RELEASE_AND_QT_VERION );
     
     QString about_info = ""
             "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\np, li { white-space: pre-wrap; }\n</style></head><body style=\" font-family:'Sans Serif'; font-size:9pt; font-weight:400; font-style:normal;\">\n<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br>   Project Home: <a href=\"http://nullget.sourceforge.net/\">http://nullget.sourceforge.net/</a>    <br><br>   NullFXP : It is a SFTP Client based on Qt Library. I hope it is useful for you. It can be compiled and run on Linux/X11, FreeBSD/X11 and Windows Mingw platform now.       </body></p></body></html>"
