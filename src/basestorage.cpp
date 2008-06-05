460b881da402e2721821090c39f0882fc96375ad
liuguangzhao <liuguangzhao@localhost>
2008-06-05 05:44:14 +0000

    storage class file fmt

--- src/basestorage.cpp
+++ src/basestorage.cpp
@@ -25,24 +25,6 @@
 // 
 // 
 // 
-// 
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License as
-// published by the Free Software Foundation; either version 3, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this program; see the file COPYING.  If not, write to
-// the Free Software Foundation, Inc., 51 Franklin Street, Fifth
-// Floor, Boston, MA 02110-1301, USA.
-
-// 
-// 
 
 // Code:
 
@@ -53,124 +35,124 @@ BaseStorage::BaseStorage()
 
 BaseStorage::~BaseStorage()
 {
-  if(this->opened)
-    this->close();
+    if(this->opened)
+        this->close();
 }
 
 bool BaseStorage::open()
 {
 #ifdef Q_OS_WIN
-  QString config_path = QCoreApplication::applicationDirPath() + QString("/.nullfxp");
+    QString config_path = QCoreApplication::applicationDirPath() + QString("/.nullfxp");
 #elif Q_OS_MAC
-  //TODO should where?
-  QString config_path = QDir::homePath()+QString("/.nullfxp");
+    //TODO should where?
+    QString config_path = QDir::homePath()+QString("/.nullfxp");
 #else
-  QString config_path = QDir::homePath()+QString("/.nullfxp");
+    QString config_path = QDir::homePath()+QString("/.nullfxp");
 #endif
 
-  if(!QDir().exists(config_path))
-    if(!QDir().mkdir(config_path)) return false;
+    if(!QDir().exists(config_path))
+        if(!QDir().mkdir(config_path)) return false;
 
-  QFile *fp = new QFile(config_path + QString("/hosts.db"));
-  fp->open(QIODevice::ReadWrite|QIODevice::Unbuffered);
-  this->opened = true;
-  this->changed = false;
-  this->ioStream.setDevice(fp);
-  this->ioStream>>this->hosts;
-  return true;
+    QFile *fp = new QFile(config_path + QString("/hosts.db"));
+    fp->open(QIODevice::ReadWrite|QIODevice::Unbuffered);
+    this->opened = true;
+    this->changed = false;
+    this->ioStream.setDevice(fp);
+    this->ioStream>>this->hosts;
+    return true;
 }
 
 bool BaseStorage::close()
 {
-  QFile * fp = (QFile*)this->ioStream.device();
-  if(this->changed)
-    this->save();
-  if(!fp->isOpen())
-    fp->close();
-  this->opened = false;
-  return true;
-  delete fp;
+    QFile * fp = (QFile*)this->ioStream.device();
+    if(this->changed)
+        this->save();
+    if(!fp->isOpen())
+        fp->close();
+    this->opened = false;
+    return true;
+    delete fp;
 }
 
 bool BaseStorage::save()
 {
-  if(!this->changed) return true;
-  QFile * fp = (QFile*)this->ioStream.device();
-  fp->resize(0);
-  this->ioStream<<this->hosts;
+    if(!this->changed) return true;
+    QFile * fp = (QFile*)this->ioStream.device();
+    fp->resize(0);
+    this->ioStream<<this->hosts;
 
-  return true;
+    return true;
 }
 bool BaseStorage::addHost(QMap<QString,QString> host)
 {
-  QMap<QString, QMap<QString,QString> >::iterator it;
+    QMap<QString, QMap<QString,QString> >::iterator it;
 
-  for(it = this->hosts.begin(); it != this->hosts.end(); it ++)
+    for(it = this->hosts.begin(); it != this->hosts.end(); it ++)
     {
-      if((*it)["show_name"] == host["show_name"]) return false;
+        if((*it)["show_name"] == host["show_name"]) return false;
     }
-  this->hosts[host["show_name"]] = host;
-  this->changed = true;
-  return true;
+    this->hosts[host["show_name"]] = host;
+    this->changed = true;
+    return true;
 }
 bool BaseStorage::removeHost(QString show_name)
 {
-  if(this->containsHost(show_name))
+    if(this->containsHost(show_name))
     {
-      this->hosts.remove(show_name);
-      this->changed = true;
-      return true;
+        this->hosts.remove(show_name);
+        this->changed = true;
+        return true;
     }
-  return false;
+    return false;
 }
 
 bool BaseStorage::updateHost(QMap<QString,QString> host)
 {
-  if(this->containsHost(host["show_name"]))
-    this->hosts[host["show_name"]] = host;
-  else
-    this->hosts[host["show_name"]] = host;
-  this->changed = true;
-  return true;
+    if(this->containsHost(host["show_name"]))
+        this->hosts[host["show_name"]] = host;
+    else
+        this->hosts[host["show_name"]] = host;
+    this->changed = true;
+    return true;
 }
 bool BaseStorage::clearHost()
 {
-  this->hosts.clear();
-  this->save();
-  return true;
+    this->hosts.clear();
+    this->save();
+    return true;
 }
 
 bool BaseStorage::containsHost(QString show_name)
 {
-  QMap<QString, QMap<QString,QString> >::iterator it;
+    QMap<QString, QMap<QString,QString> >::iterator it;
 
-  for(it = this->hosts.begin(); it != this->hosts.end(); it ++)
+    for(it = this->hosts.begin(); it != this->hosts.end(); it ++)
     {
-      if((*it)["show_name"] == show_name) return true;
+        if((*it)["show_name"] == show_name) return true;
     }
-  return false;
+    return false;
 }
 
 QMap<QString, QMap<QString,QString> > & BaseStorage::getAllHost()
 {
-  if(!this->opened) 
-    this->open();
-  return this->hosts;
+    if(!this->opened) 
+        this->open();
+    return this->hosts;
 }
 
 QMap<QString,QString> BaseStorage::getHost(QString show_name)
 {
-  QMap<QString,QString> host;
+    QMap<QString,QString> host;
 
-  if(this->containsHost(show_name))
-    return this->hosts[show_name];
-  else
-    return host;
+    if(this->containsHost(show_name))
+        return this->hosts[show_name];
+    else
+        return host;
 }
 
 int BaseStorage::hostCount()
 {
-  return this->hosts.count();
+    return this->hosts.count();
 }
 
 // 
--- src/basestorage.h
+++ src/basestorage.h
@@ -26,23 +26,6 @@
  * 
  */
 
-/* This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 3, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; see the file COPYING.  If not, write to
- * the Free Software Foundation, Inc., 51 Franklin Street, Fifth
- * Floor, Boston, MA 02110-1301, USA.
-
- */
-
 /* Code: */
 
 
@@ -53,32 +36,33 @@
 
 class BaseStorage
 {
- public:
-  BaseStorage();
-  ~BaseStorage();
-  bool open();
-  bool close();
-  bool save();
+public:
+    BaseStorage();
+    ~BaseStorage();
+    bool open();
+    bool close();
+    bool save();
 
-  bool addHost(QMap<QString,QString> host);
-  bool removeHost(QString show_name);
-  bool updateHost(QMap<QString,QString> host);
-  bool clearHost();
+    bool addHost(QMap<QString,QString> host);
+    bool removeHost(QString show_name);
+    bool updateHost(QMap<QString,QString> host);
+    bool clearHost();
 
-  bool containsHost(QString show_name);
+    bool containsHost(QString show_name);
 
-  QMap<QString, QMap<QString,QString> > & getAllHost();
-  QMap<QString,QString> getHost(QString show_name);
-  int hostCount();
+    QMap<QString, QMap<QString,QString> > & getAllHost();
+    QMap<QString,QString> getHost(QString show_name);
+    int hostCount();
   
- signals:
-  void hostListChanged();
-  void hostLIstChanged(QString show_name);
- private:
-  QMap<QString,QMap<QString,QString> >  hosts;
-  bool opened;
-  bool changed;
-  QDataStream  ioStream;
+signals:
+    void hostListChanged();
+    void hostLIstChanged(QString show_name);
+private:
+    QMap<QString,QMap<QString,QString> >  hosts;
+    bool opened;
+    bool changed;
+    QDataStream  ioStream;
+
 };
 
 /* basestorage.h ends here */
