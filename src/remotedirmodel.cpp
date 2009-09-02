a956b0dfd2c0ab89852965c58104e46e5253e994
liuguangzhao <liuguangzhao@localhost>
2009-09-02 03:36:31 +0000

    downgrade qt version required

--- src/remotedirmodel.cpp
+++ src/remotedirmodel.cpp
@@ -322,7 +322,7 @@ QVariant RemoteDirModel::data(const QModelIndex &index, int role) const
                 suffix = suffix.toLower();
                 // qDebug()<<suffix<<gMimeHash.value(suffix);
                 QIcon icon = QIcon(qApp->applicationDirPath() + "/icons/mimetypes/" + gMimeHash.value(suffix) + ".png");
-                if (icon.availableSizes().isEmpty() || gMimeHash.value(suffix).isEmpty()) {
+                if (icon.actualSize(QSize(32,32)).isEmpty() || gMimeHash.value(suffix).isEmpty()) {
                     icon = qApp->style()->standardIcon(QStyle::SP_FileIcon);
                 }
                 return icon;
