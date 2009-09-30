0219baa089c09cfcc86adfa652e7888f01795604
liuguangzhao <liuguangzhao@localhost>
2009-09-30 03:13:11 +0000

    fixed: win32 unsupport #elifdef problem

--- src/basestorage.cpp
+++ src/basestorage.cpp
@@ -31,12 +31,15 @@ BaseStorage::~BaseStorage()
 
 bool BaseStorage::init()
 {
-#ifdef Q_OS_WIN
-    this->storagePath = QCoreApplication::applicationDirPath() + QString("/.nullfxp/sessions");
-#elifdef Q_OS_MAC
-    this->storagePath = QDir::homePath()+QString("/.nullfxp/sessions");
+#ifdef Q_OS_MAC
+		this->storagePath = QDir::homePath()+QString("/.nullfxp/sessions");
 #else
+# ifdef Q_OS_WIN32
+    this->storagePath = QCoreApplication::applicationDirPath() + QString("/.nullfxp/sessions");
+#else
+		qDebug()<<"else????";
     this->storagePath = QDir::homePath()+QString("/.nullfxp/sessions");
+#endif
 #endif
 
     if (!QDir().exists(this->storagePath)) {
