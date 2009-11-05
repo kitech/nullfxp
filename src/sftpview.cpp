c8954035ed656dd1fd7a69de5bbf537ffceede2c
liuguangzhao <liuguangzhao@localhost>
2009-11-05 15:17:36 +0000

    sftpview $ line propset

--- src/sftpview.cpp
+++ src/sftpview.cpp
@@ -1,10 +1,10 @@
-// remoteview.cpp --- 
+// sftpview.cpp --- 
 // 
 // Author: liuguangzhao
 // Copyright (C) 2007-2010 liuguangzhao@users.sf.net
 // URL: http://www.qtchina.net http://nullget.sourceforge.net
 // Created: 2008-05-05 21:49:36 +0800
-// Version: $Id: remoteview.cpp 567 2009-11-01 08:12:00Z liuguangzhao $
+// Version: $Id: sftpview.cpp 567 2009-11-01 08:12:00Z liuguangzhao $
 // 
 
 #include <QtCore>
