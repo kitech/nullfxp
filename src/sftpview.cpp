99e02823aa7851c6317f34fa426bfc15a0f74053
liuguangzhao <liuguangzhao@localhost>
2010-03-20 06:02:32 +0000

    keywords set

--- src/sftpview.cpp
+++ src/sftpview.cpp
@@ -4,7 +4,7 @@
 // Copyright (C) 2007-2010 liuguangzhao@users.sf.net
 // URL: http://www.qtchina.net http://nullget.sourceforge.net
 // Created: 2008-05-05 21:49:36 +0800
-// Version: $Id: sftpview.cpp 567 2009-11-01 08:12:00Z liuguangzhao $
+// Version: $Id$
 // 
 
 #include <QtCore>
