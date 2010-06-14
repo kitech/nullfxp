b809d96476b8ffddbe332caf03699d707ed84b29
liuguangzhao <liuguangzhao@localhost>
2010-06-14 14:19:09 +0000

    improve debian/ubuntu package script.

--- packages/mkdeb.sh
+++ packages/mkdeb.sh
@@ -26,31 +26,58 @@ else
     NXVER=$TMPNXVER
 fi
 # echo $NXVER
-
+NXARCH=`arch`
+for ha in i686 i586 i486
+do
+    if [ x"$NXARCH" = x"$ha" ] ; then
+	NXARCH=i386
+	break;
+    fi
+done
 # exit;
 
-PKGDIR=/tmp/nullfxp-$NXVER-0
-rm -fvr /tmp/nullfxp-$NXVER-0
+PKGDIR=/tmp/nullfxp-$NXVER-$NXARCH-0
+rm -fr /tmp/nullfxp-$NXVER-$NXARCH-0
 mkdir $PKGDIR
 
 cp -va $NXROOT/packages/debian/DEBIAN $PKGDIR/
 rm -fv $PKGDIR/DEBIAN/.svn
 
-mkdir -pv $PKGDIR/usr/share/applications $PKGDIR/usr/bin $PKGDIR/usr/share/nullfxp
+mkdir -pv $PKGDIR/usr/share/applications $PKGDIR/usr/bin $PKGDIR/usr/share/nullfxp/docs
+mkdir -pv $PKGDIR/usr/share/nullfxp/translations
+
+for ic in 16 22 24 32 48 64 128
+do
+    mkdir -pv "$PKGDIR/usr/share/icons/hicolor/${ic}x${ic}/apps/"
+    cp -v $NXROOT/src/icons/nullget-1.png "$PKGDIR/usr/share/icons/hicolor/${ic}x${ic}/apps/nullfxp.png"
+done
+
 cp -v $NXROOT/bin/nullfxp $PKGDIR/usr/bin/
+cp -v $NXROOT/bin/touch.exe $PKGDIR/usr/bin/
+cp -v $NXROOT/bin/unitest $PKGDIR/usr/bin/
 cp -v $NXROOT/src/data/nullfxp.desktop $PKGDIR/usr/share/applications/
+cp -v $NXROOT/AUTHORS $PKGDIR/usr/share/nullfxp/docs/
+cp -v $NXROOT/ChnageLog $PKGDIR/usr/share/nullfxp/docs/
+cp -v $NXROOT/NEWS $PKGDIR/usr/share/nullfxp/docs/
+cp -v $NXROOT/INSTALL $PKGDIR/usr/share/nullfxp/docs/
+cp -v $NXROOT/TODO $PKGDIR/usr/share/nullfxp/docs/
+cp -v $NXROOT/README $PKGDIR/usr/share/nullfxp/docs/
 
 cp -va $NXROOT/src/icons $PKGDIR/usr/share/nullfxp/
 find $PKGDIR/ -name .svn | xargs rm -vfr
+
+
+pmkdir -pv $PKGDIR/usr/share/icons/hicolor/
 
 PNXSIZE=`du -s /tmp/nullfxp-$NXVER-0 | awk '{print $1}'`
 # echo $PNXSIZE
 
 sed -i "s@NXVER@$NXVER@g" $PKGDIR/DEBIAN/control
 sed -i "s@NXSIZE@$PNXSIZE@g" $PKGDIR/DEBIAN/control
+sed -i "s@NXARCH@$NXARCH@g" $PKGDIR/DEBIAN/control
 
 cd $PKGDIR/../
-dpkg -b nullfxp-$NXVER-0
+dpkg -b nullfxp-$NXVER-$NXARCH-0
 # tar zcvf nullfxp-$NXVER.tar.gz nullfxp-$NXVER-0
 
 # cleanup
