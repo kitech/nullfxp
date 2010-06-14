ba3a8a28aaac9a7bcaef6a4a74b207724f0717f6
liuguangzhao <liuguangzhao@localhost>
2010-06-14 12:31:27 +0000

    improve debain pkg maker

--- packages/mkdeb.sh
+++ packages/mkdeb.sh
@@ -1,25 +1,60 @@
 #!/bin/sh
-#
-#Author: liuguangzhao@users.sf.net
+# mkdeb.sh --- 
+# 
+# Author: liuguangzhao
+# Copyright (C) 2007-2010 liuguangzhao@users.sf.net
+# URL: 
+# Created: 2010-06-14 18:34:13 +0800
+# Version: $Id$
+# 
 
-PKGDIR=/tmp/nullfxp-2.0.1-0
-rm -fvr /tmp/nullfxp-2.0.1-0
+SHPATH=`readlink -f $0`
+NXROOT=`dirname $SHPATH`
+NXROOT="$NXROOT/.."
+NXROOT=`readlink -f $NXROOT`
+# echo $NXROOT
+
+NXROOTBASENAME=`basename $NXROOT`
+
+# echo $NXROOTBASENAME
+
+TMPNXVER=`echo $NXROOTBASENAME | awk -F\- '{print $2}'`
+if [ x"$TMPNXVER" = x"svn" ] ; then
+#    NXVER="r666"
+    NXVER=r`LC_ALL=C svn info |grep Revision|grep -v grep| awk '{print $2}'`
+else
+    NXVER=$TMPNXVER
+fi
+# echo $NXVER
+
+# exit;
+
+PKGDIR=/tmp/nullfxp-$NXVER-0
+rm -fvr /tmp/nullfxp-$NXVER-0
 mkdir $PKGDIR
 
-cp -va debian/DEBIAN $PKGDIR/
+cp -va $NXROOT/packages/debian/DEBIAN $PKGDIR/
 rm -fv $PKGDIR/DEBIAN/.svn
 
 mkdir -pv $PKGDIR/usr/share/applications $PKGDIR/usr/bin $PKGDIR/usr/share/nullfxp
-cp -v ../bin/nullfxp $PKGDIR/usr/bin/
+cp -v $NXROOT/bin/nullfxp $PKGDIR/usr/bin/
+cp -v $NXROOT/src/data/nullfxp.desktop $PKGDIR/usr/share/applications/
 
-cp -va ../src/icons $PKGDIR/usr/share/nullfxp/
-find $PKGDIR/usr/share/nullfxp/ -name .svn | xargs rm -vfr
+cp -va $NXROOT/src/icons $PKGDIR/usr/share/nullfxp/
+find $PKGDIR/ -name .svn | xargs rm -vfr
+
+PNXSIZE=`du -s /tmp/nullfxp-$NXVER-0 | awk '{print $1}'`
+# echo $PNXSIZE
+
+sed -i "s@NXVER@$NXVER@g" $PKGDIR/DEBIAN/control
+sed -i "s@NXSIZE@$PNXSIZE@g" $PKGDIR/DEBIAN/control
 
 cd $PKGDIR/../
-dpkg -b nullfxp-2.0.1-0
+dpkg -b nullfxp-$NXVER-0
+# tar zcvf nullfxp-$NXVER.tar.gz nullfxp-$NXVER-0
 
 # cleanup
-rm -fvr $PKGDIR
+# rm -fvr $PKGDIR
 
 # come back
 cd -
