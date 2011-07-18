f985a75ab519aa8b417a3c3d4ed1bd2d034ef086
liuguangzhao <liuguangzhao@localhost>
2011-07-18 03:15:02 +0000

    libssh win32 comple ok. dll mode and static mode both ok.

--- src/libssh/libssh/config.h
+++ src/libssh/libssh/config.h
@@ -1,127 +1,25 @@
-/* Name of package */
-#define PACKAGE "libssh"
 
-/* Version number of package */
-#define VERSION "0.4.6"
-
-/* #undef LOCALEDIR */
-#define DATADIR "/usr/local/share/libssh"
-#define LIBDIR "/usr/local/lib"
-#define PLUGINDIR "/usr/local/lib/libssh-4"
-#define SYSCONFDIR "/usr/local/etc"
-#define BINARYDIR "/home/gzleo/temp/libssh-0.4.6/build"
-#define SOURCEDIR "/home/gzleo/temp/libssh-0.4.6"
-
-/************************** HEADER FILES *************************/
-
-/* Define to 1 if you have the <argp.h> header file. */
-#define HAVE_ARGP_H 1
-
-/* Define to 1 if you have the <pty.h> header file. */
-#define HAVE_PTY_H 1
-
-/* Define to 1 if you have the <termios.h> header file. */
-/* #undef HAVE_TERMIOS_H */
-
-/* Define to 1 if you have the <openssl/aes.h> header file. */
-#define HAVE_OPENSSL_AES_H 1
-
-/* Define to 1 if you have the <wspiapi.h> header file. */
-/* #undef HAVE_WSPIAPI_H */
-
-/* Define to 1 if you have the <openssl/blowfish.h> header file. */
-#define HAVE_OPENSSL_BLOWFISH_H 1
-
-/* Define to 1 if you have the <openssl/des.h> header file. */
-#define HAVE_OPENSSL_DES_H 1
-
-/*************************** FUNCTIONS ***************************/
 
 /* Define to 1 if you have the `snprintf' function. */
-#define HAVE_SNPRINTF 1
+// #define HAVE_SNPRINTF 1
+// #define HAVE__SNPRINTF 1
+#define HAVE__SNPRINTF_S 1
 
-/* Define to 1 if you have the `_snprintf' function. */
-/* #undef HAVE__SNPRINTF */
-
-/* Define to 1 if you have the `_snprintf_s' function. */
-/* #undef HAVE__SNPRINTF_S */
 
 /* Define to 1 if you have the `vsnprintf' function. */
-#define HAVE_VSNPRINTF 1
+#define HAVE__VSNPRINTF 1
 
-/* Define to 1 if you have the `_vsnprintf' function. */
-/* #undef HAVE__VSNPRINTF */
-
-/* Define to 1 if you have the `_vsnprintf_s' function. */
-/* #undef HAVE__VSNPRINTF_S */
-
-/* Define to 1 if you have the `snprintf' function. */
-#define HAVE_SNPRINTF 1
-
-/* Define to 1 if you have the `_snprintf' function. */
-/* #undef HAVE__SNPRINTF */
-
-/* Define to 1 if you have the `_snprintf_s' function. */
-/* #undef HAVE__SNPRINTF_S */
-
-
-/* Define to 1 if you have the `strncpy' function. */
-#define HAVE_STRNCPY 1
-
-/* Define to 1 if you have the `cfmakeraw' function. */
-#define HAVE_CFMAKERAW 1
 
 /* Define to 1 if you have the `getaddrinfo' function. */
 #define HAVE_GETADDRINFO 1
 
-/* Define to 1 if you have the `gethostbyname' function. */
-#define HAVE_GETHOSTBYNAME 1
-
-/* Define to 1 if you have the `poll' function. */
-#define HAVE_POLL 1
-
 /* Define to 1 if you have the `select' function. */
 #define HAVE_SELECT 1
 
-/* Define to 1 if you have the `regcomp' function. */
-#define HAVE_REGCOMP 1
-
-/*************************** LIBRARIES ***************************/
 
 /* Define to 1 if you have the `crypto' library (-lcrypto). */
 #define HAVE_LIBCRYPTO 1
 
-/* Define to 1 if you have the `gcrypt' library (-lgcrypt). */
-/* #undef HAVE_LIBGCRYPT */
 
 /* Define to 1 if you have the `z' library (-lz). */
 #define HAVE_LIBZ 1
-
-/**************************** OPTIONS ****************************/
-
-/* Define to 1 if you want to enable ZLIB */
-#define WITH_LIBZ 1
-
-/* Define to 1 if you want to enable SFTP */
-#define WITH_SFTP 1
-
-/* Define to 1 if you want to enable SSH1 */
-/* #undef WITH_SSH1 */
-
-/* Define to 1 if you want to enable server support */
-#define WITH_SERVER 1
-
-/* Define to 1 if you want to enable debug output for crypto functions */
-/* #undef DEBUG_CRYPTO */
-
-/* Define to 1 if you want to enable pcap output support (experimental) */
-/* #undef WITH_PCAP */
-
-/* Define to 1 if you want to enable calltrace debug output */
-#define DEBUG_CALLTRACE 1
-
-/*************************** ENDIAN *****************************/
-
-/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
-   significant byte first (like Motorola and SPARC, unlike Intel). */
-/* #undef WORDS_BIGENDIAN */
--- src/libssh/libssh/libssh.pro
+++ src/libssh/libssh/libssh.pro
@@ -12,6 +12,7 @@ TEMPLATE = lib
 TARGET = ssh
 
 CONFIG += dll console #staticlib
+#CONFIG +=  console staticlib
 CONFIG -= qt 
 OBJECTS_DIR = obj
 
@@ -62,6 +63,7 @@ SOURCES +=   agent.c \
   scp.c \
   socket.c \
   string.c \
+  server.c \
   threads.c \
   wrapper.c
 
@@ -80,6 +82,7 @@ win32 {
 # controll if show libssh debug message, using this line
 # DEFINES += LIBSSH2DEBUG=1 
 
+
 win32 {
     !win32-g++ {
         DEFINES += LIBSSH2_WIN32 _CRT_SECURE_NO_DEPRECATE
@@ -88,10 +91,13 @@ win32 {
         VAMD64=$$find(CLARCH,amd64)
         isEmpty(VAMD64) {
             INCLUDEPATH += Z:/librarys/vc-ssl-x86/include Z:/librarys/vc-zlib/include
+             QMAKE_LIBDIR += Z:/librarys/vc-ssl-x86/lib Z:/librarys/vc-zlib/static32
         } else {
             INCLUDEPATH += Z:/librarys/vc-ssl-x64/include Z:/librarys/vc-zlib/include
+             QMAKE_LIBDIR += Z:/librarys/vc-ssl-x64/lib Z:/librarys/vc-zlib/staticx64
         }
     }
+    LIBS += -lzlibstat -llibeay32 -lssleay32 -ladvapi32 -luser32 -lws2_32 -lgdi32 -lshell32
 } else {
      LIBS += -lssl -lcrypto -lz
 }
